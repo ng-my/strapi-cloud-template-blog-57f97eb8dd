0ef8768dcc52043743c828828ca8702d67c095ed
yhq_ng <yanghongqing@tanmarket.cn>
2025-09-03 10:47:13 +0800

    yhq https://typical-sharing-fb0a62d1ce.strapiapp.com/admin/plugins/content-type-builder/content-types/api::about.about

--- config/server.js
+++ config/server.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
     keys: env.array('APP_KEYS'),
   },
   // 设置生产环境的完整外部 URL
-  url: env('MY_HEROKU_URL'), // 确保环境变量 MY_HEROKU_URL 已设置
+  url: env('https://typical-sharing-fb0a62d1ce.strapiapp.com'), // 确保环境变量 MY_HEROKU_URL 已设置
   admin: {
     // 允许在生产环境访问 Admin 面板的 Content-Type Builder 等功能
     allowAdminGeneratedContent: true,
