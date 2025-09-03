b7fbc41e1ad5edff0b5b29c27c04eb1a09df24dd
yhq_ng <yanghongqing@tanmarket.cn>
2025-09-03 10:38:06 +0800

    yhq url: env('MY_HEROKU_URL'), // 确保环境变量 MY_HEROKU_URL 已设置

--- config/server.js
+++ config/server.js
@@ -4,6 +4,8 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  // 设置生产环境的完整外部 URL
+  url: env('MY_HEROKU_URL'), // 确保环境变量 MY_HEROKU_URL 已设置
   admin: {
     // 允许在生产环境访问 Admin 面板的 Content-Type Builder 等功能
     allowAdminGeneratedContent: true,
