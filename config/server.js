1752f485be9eb13e0356c3795251aebb24e49a2c
yhq_ng <yanghongqing@tanmarket.cn>
2025-09-03 10:23:30 +0800

    yhq admin: {     // 允许在生产环境访问 Admin 面板的 Content-Type Builder 等功能     allowAdminGeneratedContent: true,   },

--- config/server.js
+++ config/server.js
@@ -4,6 +4,10 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  admin: {
+    // 允许在生产环境访问 Admin 面板的 Content-Type Builder 等功能
+    allowAdminGeneratedContent: true,
+  },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
