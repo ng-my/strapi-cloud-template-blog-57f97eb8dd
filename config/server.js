f831dd06180be6360793e9528804347f9a70d8c4
yhq_ng <yanghongqing@tanmarket.cn>
2025-09-03 11:12:47 +0800

    yhq 添加Teacher

--- config/server.js
+++ config/server.js
@@ -4,12 +4,6 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // 设置生产环境的完整外部 URL
-  url: env('https://typical-sharing-fb0a62d1ce.strapiapp.com'), // 确保环境变量 MY_HEROKU_URL 已设置
-  admin: {
-    // 允许在生产环境访问 Admin 面板的 Content-Type Builder 等功能
-    allowAdminGeneratedContent: true,
-  },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
